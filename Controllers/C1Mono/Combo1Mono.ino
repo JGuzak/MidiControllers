0b40f85a916390ae2cd9fdd0b1f5fde8fcd45979
Jordan Guzak <jguzak@live.com>
2017-10-12 02:48:14 -0400

    formatting

--- Controllers/C1Mono/Combo1Mono.ino
+++ Controllers/C1Mono/Combo1Mono.ino
@@ -384,7 +384,7 @@ void buttonHandler() {
 
     // check for shift mode
     if (shiftMode) {
-        for (int i = 0; i < (NUM_ENCODERS-1); i++) {
+        for (int i = 0; i < (NUM_ENCODERS - 1); i++) {
             if (io.digitalRead(buttonPin[i+1]) == 0) {
                 curBank = i;
             }
