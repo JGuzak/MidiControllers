c6e3706c34e633b9b21e005aa1716b12c9f77ca5
Jordan Guzak <JGuzak@live.com>
2018-03-23 14:10:14 -0700

    setup lufa library and compiled new firmware for C1Mono

---
+++ Controllers/C1Mono/hiduino_C1Mono/C1Mono.d
@@ -0,0 +1,311 @@
+C1Mono.o: C1Mono.c C1Mono.h Descriptors.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/USB.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/Architectures.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/BoardTypes.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/CompilerSpecific.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/Attributes.h \
+ Config/LUFAConfig.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/Endianness.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBMode.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/../../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBTask.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBMode.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBController.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Device.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBController.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/Events.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/StdRequestType.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/StdDescriptors.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/Events.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/StdDescriptors.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/ConfigDescriptors.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/HostStandardReq.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBController.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBInterrupt.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/Device.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/Endpoint.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/EndpointStream.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/AndroidAccessoryClass.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/../Core/USBMode.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/AudioClass.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/AudioClassDevice.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../../USB.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/CDCClass.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/CDCClassDevice.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/HIDClass.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/HIDClassDevice.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/MassStorageClass.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/MassStorageClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/MIDIClass.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/MIDIClassDevice.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/MIDIClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/PrinterClass.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/PrinterClassDevice.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/PrinterClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/RNDISClass.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/RNDISClassDevice.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/RNDISClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/StillImageClass.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/Board/LEDs.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/Board/../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/Board/AVR8/UNO/LEDs.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/Board/AVR8/UNO/../../../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/Serial.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/AVR8/Serial_AVR8.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/AVR8/../../../Common/Common.h \
+ ../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/AVR8/../../Misc/TerminalCodes.h \
+ ../../LUFA-170418/LUFA/../LUFA/Platform/Platform.h \
+ ../../LUFA-170418/LUFA/../LUFA/Platform/../Common/Common.h
+
+C1Mono.h:
+
+Descriptors.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/USB.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/Architectures.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/BoardTypes.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/CompilerSpecific.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/Attributes.h:
+
+Config/LUFAConfig.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/../../Common/Endianness.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBMode.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/../../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBTask.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBMode.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBController.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Device.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBController.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/Events.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/StdRequestType.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/Events.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/ConfigDescriptors.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/HostStandardReq.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBController.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/USBInterrupt.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/Device.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/Endpoint.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Core/EndpointStream.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/AndroidAccessoryClass.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/../Core/USBMode.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/AudioClass.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/AudioClassDevice.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../../USB.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/CDCClass.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/CDCClassDevice.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/HIDClass.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/HIDClassDevice.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/MassStorageClass.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/MassStorageClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/MIDIClass.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/MIDIClassDevice.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/MIDIClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/PrinterClass.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/PrinterClassDevice.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/PrinterClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/RNDISClass.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/RNDISClassDevice.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/RNDISClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/USB/Class/StillImageClass.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/Board/LEDs.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/Board/../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/Board/AVR8/UNO/LEDs.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/Board/AVR8/UNO/../../../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/Serial.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/AVR8/Serial_AVR8.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/AVR8/../../../Common/Common.h:
+
+../../LUFA-170418/LUFA/../LUFA/Drivers/Peripheral/AVR8/../../Misc/TerminalCodes.h:
+
+../../LUFA-170418/LUFA/../LUFA/Platform/Platform.h:
+
+../../LUFA-170418/LUFA/../LUFA/Platform/../Common/Common.h:
